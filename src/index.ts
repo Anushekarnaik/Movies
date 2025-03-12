ec54eccd2caa77dc5f07fc6aa3397f1f93581576
Anushekarnaik <anushekarnaik2003@gmail.com>
2025-03-12 10:25:10 +0530

    refactor movie management implementation to use functional approach and improve code organization

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,26 @@
-import { MovieManager } from "./movies_management";
+import { addMovie, rateMovie, getAverageRating, getTopRatedMovies, getMoviesByGenre, getMoviesByDirector, searchMoviesBasedOnKeyword, getMovie, removeMovie } from "./movies_management";
 
-const movieManager = new MovieManager();
+// Adding movies
+addMovie("1", "Inception", "Christopher Nolan", 2010, "Sci-Fi");
+addMovie("2", "Interstellar", "Christopher Nolan", 2014, "Sci-Fi");
+addMovie("3", "The Dark Knight", "Christopher Nolan", 2008, "Action");
 
+// Rating movies
+rateMovie("1", 5);
+rateMovie("2", 4);
+rateMovie("3", 5);
+rateMovie("1", 4);
 
-movieManager.addMovie("1", "Inception", "Christopher Nolan", 2010, "Sci-Fi");
-movieManager.addMovie("2", "Interstellar", "Christopher Nolan", 2014, "Sci-Fi");
-movieManager.addMovie("3", "The Dark Knight", "Christopher Nolan", 2008, "Action");
+// Fetching details
+console.log("Average rating of Inception:", getAverageRating("1"));
+console.log("Top rated movies:", getTopRatedMovies());
+console.log("Movies by genre 'Sci-Fi':", getMoviesByGenre("Sci-Fi"));
+console.log("Movies by Christopher Nolan:", getMoviesByDirector("Christopher Nolan"));
+console.log("Search movies with 'Dark':", searchMoviesBasedOnKeyword("Dark"));
 
+// Fetching a specific movie
+console.log("Details of movie with ID 1:", getMovie("1"));
 
-movieManager.rateMovie("1", 5);
-movieManager.rateMovie("2", 4);
-movieManager.rateMovie("3", 5);
-movieManager.rateMovie("1", 4);
-
-
-console.log("Average rating of Inception:", movieManager.getAverageRating("1"));
-console.log("Top rated movies:", movieManager.getTopRatedMovies());
-console.log("Movies by genre 'Sci-Fi':", movieManager.getMoviesByGenre("Sci-Fi"));
-console.log("Movies by Christopher Nolan:", movieManager.getMoviesByDirector("Christopher Nolan"));
-console.log("Search movies with 'Dark':", movieManager.searchMoviesBasedOnKeyword("Dark"));
-
-
-console.log("Details of movie with ID 1:", movieManager.getMovie("1"));
-
-
-
-movieManager.removeMovie("2");
-console.log("Movies after removing ID 2:", movieManager.getTopRatedMovies());
+// Removing a movie
+removeMovie("2");
+console.log("Movies after removing ID 2:", getTopRatedMovies());
